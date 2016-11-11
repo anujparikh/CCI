a5ab5596cf20198b601e69457008123099ed09c6
anujparikh <anujparikh1@gmail.com>
2016-11-11 01:40:48 -0500

    Element from Array

--- src/main/java/LoneElementInArray.java
+++ src/main/java/LoneElementInArray.java
@@ -9,17 +9,18 @@
  * -- using memory.
  * -- not using any external memory.
  * eg: [2,1,4,5,1,4,2,2,4,1]
+ * https://www.careercup.com/question?id=7902674
  */
 public class LoneElementInArray {
     public int findElementThatAppearsOnce(int[] input) {
         int ones = 0;
         int twos = 0;
         for (int i = 0; i < input.length; i++) {
-            twos |= ones & input[i];
-            ones ^= input[i];
-            int not_threes = ~(ones & twos);
-            ones &= not_threes;
-            twos &= not_threes;
+            twos |= ones & input[i]; // add it to twos if it exists in ones
+            ones ^= input[i]; // it exists in ones, remove it, otherwise, add it
+            int not_threes = ~(ones & twos); //if current is in ones and twice, add it to Threes
+            ones &= not_threes; //remove current from ones
+            twos &= not_threes; //remove current from twos
         }
         return ones;
     }
