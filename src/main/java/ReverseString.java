2859a7b7167428b699348d399f1dca052b8cea6b
anujparikh <anujparikh1@gmail.com>
2016-11-14 23:34:06 -0500

    Updated Reverse String

--- src/main/java/ReverseString.java
+++ src/main/java/ReverseString.java
@@ -3,10 +3,10 @@
  */
 public class ReverseString {
     public String reverseCStyleString(String input) {
-        String output = " ";
+        StringBuilder outputBuilder = new StringBuilder(" ");
         for (int i = input.length() - 1; i >= 0; i--) {
-            output += input.charAt(i);
+            outputBuilder.append(input.charAt(i));
         }
-        return output;
+        return outputBuilder.toString();
     }
 }
