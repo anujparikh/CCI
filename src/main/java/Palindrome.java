d854e082006f8f48129b41e7e357ffd8af980a5c
anujparikh <anujparikh1@gmail.com>
2016-11-21 13:36:22 -0500

    custom binary heap

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -1,16 +1,28 @@
+import java.util.stream.IntStream;
+
 public class Palindrome {
-    public boolean checkStringForPanlindrome(String input) {
+    public boolean checkStringForPalindrome(String input) {
         for (int i = 0, j = input.length() - 1; i < j; i++, j--) {
             if (input.charAt(i) != input.charAt(j)) return false;
         }
         return true;
     }
 
+    public static void checkInputForFizzBuzz(int range) {
+        IntStream.rangeClosed(1, range).forEach(k -> System.out.println(checkNumber(k)));
+    }
+
+    public static String checkNumber(int k) {
+        String result = ((k % 3 == 0 ? "Fizz" : "") + (k % 5 == 0 ? "Buzz" : ""));
+        return result.length() == 0 ? Integer.toString(k) : result;
+    }
+
     public static void main(String[] args) {
         Palindrome testObject = new Palindrome();
         String input = "abcdcba";
-        System.out.println("Input String " + input + " is Palindrome: " + testObject.checkStringForPanlindrome(input));
+        System.out.println("Input String " + input + " is Palindrome: " + testObject.checkStringForPalindrome(input));
         input = "abcdedcba";
-        System.out.println("Input String " + input + " is Palindrome: " + testObject.checkStringForPanlindrome(input));
+        System.out.println("Input String " + input + " is Palindrome: " + testObject.checkStringForPalindrome(input));
+        checkInputForFizzBuzz(20);
     }
 }
