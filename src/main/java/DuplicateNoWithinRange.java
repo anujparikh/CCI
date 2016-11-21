1528a3a62481579468b8eda8ab0c803f38197e71
anujparikh <anujparikh1@gmail.com>
2016-11-21 15:22:15 -0500

    Duplicate Number within Range

---
+++ src/main/java/DuplicateNoWithinRange.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by anujparikh on 11/21/16.
+ */
+public class DuplicateNoWithinRange {
+
+    public static boolean checkForDuplicateNoWithinGivenRange(int[] input, int range) {
+        Map<Integer, Integer> inputMap = new HashMap<>();
+        for (int i = 0; i < input.length; i++) {
+            if (inputMap.get(input[i]) != null) return true;
+            else {
+                if (i >= range) {
+                    inputMap.remove(input[i - range]);
+                }
+                inputMap.put(input[i], 1);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] input = {1, 2, 3, 4, 1, 2, 3, 4};
+        System.out.println(checkForDuplicateNoWithinGivenRange(input, 3));
+    }
+
+}
